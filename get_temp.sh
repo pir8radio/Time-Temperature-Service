2ccf0072824947b74330dde3a4187ef43ef104f3
Pir8Radio (Pirate Radio) <pir8radio@gmail.com>
2025-07-26 17:37:51 -0500

    Update get_temp.sh

--- get_temp.sh
+++ get_temp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # https://github.com/pir8radio/Time-Temperature-Service
 
-# -- EDIT THE LAT AND LON BELOW FOR THE AREA YOU WANT TO GET A TEMPERATURE VALUE FOR (lines 6 & 7) --
+# -- EDIT THE LAT AND LON BELOW FOR THE AREA YOU WANT TO GET A TEMPERATURE VALUE FOR (lines 8 & 9) --
 # --- Paths ---
 CACHE_FILE="/var/lib/asterisk/last_known_temp"
 LOG_FILE="/var/lib/asterisk/temp_script.log"
